88755b31aa1b4f58fb3234f1e05b786e79d2fb9d
Jayson Grace <jayson.e.grace@gmail.com>
2023-04-02 15:46:12 -0600

    Broke large, unwieldy function into smaller, more manageable parts

--- pkg/blocks/basicstep.go
+++ pkg/blocks/basicstep.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/facebookincubator/TTP-Runner/pkg/logging"
+	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
 
@@ -126,95 +127,132 @@ func (b *BasicStep) IsNil() bool {
 
 // Validate validates the BasicStep, checking for the necessary attributes and dependencies.
 func (b *BasicStep) Validate() error {
-	err := b.Act.Validate()
-	if err != nil {
+	// Validate Act
+	if err := b.Act.Validate(); err != nil {
+		logging.Logger.Sugar().Error(zap.Error(err))
 		return err
 	}
 
+	// Check if Inline is provided
 	if b.Inline == "" {
-		return errors.New("inline must be provided")
+		err := errors.New("inline must be provided")
+		logging.Logger.Sugar().Error(zap.Error(err))
+		return err
 	}
 
+	// Set Executor to "bash" if it is not provided
 	if b.Executor == "" && b.Inline != "" {
-		// TODO: add os handling using the runtime (ezpz)
 		logging.Logger.Sugar().Debug("defaulting to bash since executor was not provided")
 		b.Executor = "bash"
 	}
 
+	// Return if Executor is ExecutorBinary
 	if b.Executor == ExecutorBinary {
 		return nil
 	}
-	_, err = exec.LookPath(b.Executor)
-	if err != nil {
+
+	// Check if the executor is in the system path
+	if _, err := exec.LookPath(b.Executor); err != nil {
+		logging.Logger.Sugar().Error(zap.Error(err))
 		return err
 	}
+
+	// Validate CleanupStep if it is not nil
 	if b.CleanupStep != nil {
-		err := b.CleanupStep.Validate()
-		if err != nil {
+		if err := b.CleanupStep.Validate(); err != nil {
+			logging.Logger.Sugar().Errorw("error validating cleanup step", zap.Error(err))
 			return err
 		}
 	}
+
 	logging.Logger.Sugar().Debugw("command found in path", "executor", b.Executor)
 
 	return nil
 }
 
+// Execute runs the BasicStep and returns an error if any occur.
 func (b *BasicStep) Execute() (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Minute)
 	defer cancel()
 	logging.Logger.Sugar().Debugw("available data", "outputs", b.output)
+
 	logging.Logger.Sugar().Info("========= Executing ==========")
+
 	if b.Inline != "" {
-		err = b.executeBashStdin(ctx)
+		if err := b.executeBashStdin(ctx); err != nil {
+			logging.Logger.Sugar().Error(zap.Error(err))
+			return err
+		}
 	}
+
 	logging.Logger.Sugar().Info("========= Result ==========")
-	return err
+
+	return nil
 }
 
 func (b *BasicStep) executeBashStdin(ptx context.Context) (err error) {
 	ctx, cancel := context.WithCancel(ptx)
 	defer cancel()
-	cmd := exec.CommandContext(ctx, b.Executor)
-	cmd.Env = FetchEnv(b.Environment)
-	cmd.Dir = b.WorkDir
-	var inline bytes.Buffer
 
+	inline, err := b.processInlineTemplate()
+	if err != nil {
+		return err
+	}
+
+	cmd := b.prepareCommand(ctx, inline)
+
+	err = b.runCommand(cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (b *BasicStep) processInlineTemplate() (string, error) {
 	funcMap := template.FuncMap{
-		// The name "title" is what the function will be called in the template text.
 		"json": JSONString,
 	}
 
 	tmpl, err := template.New("inline").Funcs(funcMap).Parse(b.Inline)
 	if err != nil {
 		logging.Logger.Sugar().Warnw("failed to parse template", "err", err)
-		return err
+		return "", err
 	}
 
+	var inline bytes.Buffer
 	err = tmpl.Execute(&inline, b.output)
 	if err != nil {
 		logging.Logger.Sugar().Warnw("failed to execute template", "err", err)
-		return err
+		return "", err
 	}
 
 	logging.Logger.Sugar().Debugw("value of inline parsed", "inline", inline.String())
-	// if b.FetchArgs() != nil {
-	// 	inline = fmt.Sprintf("%s %s", b.Inline, strings.Join(b.FetchArgs(), " "))
-	// }
-	input := strings.NewReader(inline.String())
 
-	logging.Logger.Sugar().Debugw("check input", "input", inline)
-	cmd.Stdin = input
+	return inline.String(), nil
+}
 
+func (b *BasicStep) prepareCommand(ctx context.Context, inline string) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, b.Executor)
+	cmd.Env = FetchEnv(b.Environment)
+	cmd.Dir = b.WorkDir
+	cmd.Stdin = strings.NewReader(inline)
+
+	return cmd
+}
+
+func (b *BasicStep) runCommand(cmd *exec.Cmd) error {
 	var stdoutBuf, stderrBuf bytes.Buffer
 	cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
 	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
 
-	err = cmd.Run()
+	err := cmd.Run()
 	outStr, errStr := stdoutBuf.String(), stderrBuf.String()
 	if err != nil {
 		logging.Logger.Sugar().Warnw("bad exit of process", "stdout", outStr, "stderr", errStr, "exit code", cmd.ProcessState.ExitCode())
 		return err
 	}
+
 	logging.Logger.Sugar().Debugw("output of process", "stdout", outStr, "stderr", errStr, "status", cmd.ProcessState.ExitCode())
 
 	b.SetOutputSuccess(&stdoutBuf, cmd.ProcessState.ExitCode())
