af7ad1cd891feff6ade61fa0c648d52b6a81f6a2
Jayson Grace <jayson.e.grace@gmail.com>
2023-04-02 15:31:16 -0600

    Updated and added new tests to subttp_test

--- pkg/blocks/subttp_test.go
+++ pkg/blocks/subttp_test.go
@@ -1,6 +1,8 @@
 package blocks_test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 	"testing/fstest"
 
@@ -70,4 +72,92 @@ ttp: bad.yaml
 	if err := ttps.Validate(); err == nil {
 		t.Error("failure should occur here as file does not exist")
 	}
+}
+
+func TestSubTTPStep(t *testing.T) {
+	// Prepare a temporary TTP file for the SubTTPStep to reference
+	subTTPData := `
+---
+name: sub_ttp
+description: "SubTTP for testing SubTTPStep execution."
+steps:
+  - name: sub_ttp_step
+    file: ~/fb/opensource/TTP-Runner/tests/test.sh
+    args:
+      - sub_ttp_arg
+`
+	tmpSubTTPFile, err := os.CreateTemp(os.TempDir(), "sub_ttp_*.ttp")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	defer os.Remove(tmpSubTTPFile.Name())
+
+	os.WriteFile(tmpSubTTPFile.Name(), []byte(subTTPData), 0644)
+
+	t.Run("Test SubTTPStep unmarshalling and validation", func(t *testing.T) {
+		yamlContent := fmt.Sprintf(`
+---
+name: test_subttpstep
+description: "Test unmarshalling and validation of SubTTPStep."
+steps:
+  - name: test_sub_ttp
+    ttp: %s
+`, tmpSubTTPFile.Name())
+
+		var parsedTTP blocks.TTP
+		err := yaml.Unmarshal([]byte(yamlContent), &parsedTTP)
+		if err != nil {
+			t.Fatalf("error unmarshalling yaml: %v", err)
+		}
+
+		subTTPStep, ok := parsedTTP.Steps[0].(blocks.Step)
+		if !ok {
+			t.Fatal("error asserting step as SubTTPStep")
+		}
+
+		err = subTTPStep.Validate()
+		if err != nil {
+			t.Errorf("validation failed: %v", err)
+		}
+	})
+
+	t.Run("Test SubTTPStep with invalid TTP file path", func(t *testing.T) {
+		yamlContent := `
+---
+name: test_subttpstep_invalid_path
+description: "Test SubTTPStep with an invalid TTP file path."
+steps:
+  - name: test_sub_ttp_invalid
+    ttp: non_existent_file.ttp
+`
+
+		var parsedTTP blocks.TTP
+		err := yaml.Unmarshal([]byte(yamlContent), &parsedTTP)
+		if err != nil {
+			t.Fatalf("error unmarshalling yaml: %v", err)
+		}
+
+		subTTPStep, ok := parsedTTP.Steps[0].(blocks.Step)
+		if !ok {
+			t.Fatal("error asserting step as SubTTPStep")
+		}
+
+		err = subTTPStep.Validate()
+		if err == nil {
+			t.Error("expected validation to fail due to invalid TTP file path")
+		}
+	})
+
+	t.Run("Test SubTTPStep with empty TtpFile", func(t *testing.T) {
+		subTTPStep := &blocks.SubTTPStep{
+			Act: &blocks.Act{
+				Name: "test_sub_ttp_empty",
+			},
+		}
+
+		err := subTTPStep.Validate()
+		if err == nil {
+			t.Error("expected validation to fail due to empty TtpFile")
+		}
+	})
 }
