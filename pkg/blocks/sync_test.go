dae399bf49d52e5ce0c59f9a30364d1f56a52991
Jayson Grace <jayson.e.grace@gmail.com>
2023-04-02 15:26:16 -0600

    Add sync_test

---
+++ pkg/blocks/sync_test.go
@@ -0,0 +1,66 @@
+package blocks_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/facebookincubator/TTP-Runner/pkg/blocks"
+)
+
+func TestLoadTTP(t *testing.T) {
+	// Create a temporary file with valid TTP content.
+	validTTPData := `
+---
+name: test_load_yaml
+description: "Test loading and execution of YAML-based TTPs."
+steps:
+  - name: testing_exec
+    file: ~/fb/opensource/TTP-Runner/tests/test.sh
+    args:
+      - blah
+  - name: testing_exec_two
+    file: ~/fb/opensource/TTP-Runner/tests/test.sh
+    args:
+      - blah
+      - steps.testing_exec.output
+`
+	invalidTTPData := `
+---
+name: invalid_ttp
+description: "Test detection and handling of invalid YAML-based TTPs."
+steps:
+  - name: "step1"
+  - name: "step2"
+`
+	tmpFile1, err := os.CreateTemp(os.TempDir(), "valid_*.ttp")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	defer os.Remove(tmpFile1.Name())
+
+	tmpFile2, err := os.CreateTemp(os.TempDir(), "invalid_*.ttp")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	defer os.Remove(tmpFile2.Name())
+
+	os.WriteFile(tmpFile1.Name(), []byte(validTTPData), 0644)
+	os.WriteFile(tmpFile2.Name(), []byte(invalidTTPData), 0644)
+
+	testCases := []struct {
+		name     string
+		filename string
+		wantErr  bool
+	}{
+		{"Valid TTP", tmpFile1.Name(), false},
+		{"Invalid TTP", tmpFile2.Name(), true},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := blocks.LoadTTP(tc.filename)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("error running LoadTTP() = %v, wantErr %v", err, tc.wantErr)
+			}
+		})
+	}
+}
