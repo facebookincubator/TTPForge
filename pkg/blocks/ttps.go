7aea865bb479587549f5a31965c27751877f7399
Jayson Grace <jayson.e.grace@gmail.com>
2023-04-02 17:23:26 -0600

    Added missing function comment

--- pkg/blocks/ttps.go
+++ pkg/blocks/ttps.go
@@ -20,9 +20,17 @@ type TTP struct {
 	WorkDir string `yaml:"-"`
 }
 
-// UnmarshalYAML custom unmarshalling implementation for the TTP structure.
+// UnmarshalYAML is a custom unmarshalling implementation for the TTP structure. It decodes a YAML Node into a TTP object,
+// handling the decoding and validation of the individual steps within the TTP.
+//
+// Parameters:
+//
+// node: A pointer to a yaml.Node that represents the TTP structure to be unmarshalled.
+//
+// Returns:
+//
+// error: An error if the decoding process fails or if the TTP structure contains invalid steps.
 func (t *TTP) UnmarshalYAML(node *yaml.Node) error {
-	// TTPTmpl is a temporary structure to assist in unmarshalling a TTP object.
 	type TTPTmpl struct {
 		Name        string            `yaml:"name,omitempty"`
 		Description string            `yaml:"description"`
@@ -39,43 +47,41 @@ func (t *TTP) UnmarshalYAML(node *yaml.Node) error {
 	t.Description = tmpl.Description
 	t.Environment = tmpl.Environment
 
-	// Decode and validate each step in the TTP
-	for _, stepnode := range tmpl.Steps {
-		basic := NewBasicStep()
-		file := NewFileStep()
-		subttp := NewSubTTPStep()
-		var err, berr, ferr, serr error
+	return t.decodeAndValidateSteps(tmpl.Steps)
+}
 
-		berr = stepnode.Decode(&basic)
-		if berr == nil && !basic.IsNil() {
-			logging.Logger.Sugar().Debugw("basic", "b", basic)
-			t.Steps = append(t.Steps, basic)
-			continue
+func (t *TTP) decodeAndValidateSteps(steps []yaml.Node) error {
+	stepTypes := []Step{NewBasicStep(), NewFileStep(), NewSubTTPStep()}
+
+	for _, stepNode := range steps {
+		decoded := false
+
+		for _, stepType := range stepTypes {
+			err := stepNode.Decode(stepType)
+			if err == nil && !stepType.IsNil() {
+				logging.Logger.Sugar().Debugw("decoded step", "step", stepType)
+				t.Steps = append(t.Steps, stepType)
+				decoded = true
+				break
+			}
 		}
 
-		ferr = stepnode.Decode(&file)
-		if ferr == nil && !file.IsNil() {
-			logging.Logger.Sugar().Debugw("file", "f", file)
-			t.Steps = append(t.Steps, file)
-			continue
+		if !decoded {
+			return t.handleInvalidStepError(stepNode)
 		}
-
-		serr = stepnode.Decode(&subttp)
-		if serr == nil && !subttp.IsNil() {
-			logging.Logger.Sugar().Debugw("subttp", "s", subttp)
-			t.Steps = append(t.Steps, subttp)
-			continue
-		}
-
-		act := Act{}
-		err = stepnode.Decode(&act)
-		if act.Name != "" && err != nil {
-			return fmt.Errorf("invalid step found, missing parameters for following types: %w, %w, %w", berr, ferr, serr)
-		}
-		return fmt.Errorf("invalid step found with no name, missing parameters for following types: %w, %w, %w", berr, ferr, serr)
 	}
 
 	return nil
+}
+
+func (t *TTP) handleInvalidStepError(stepNode yaml.Node) error {
+	act := Act{}
+	err := stepNode.Decode(&act)
+
+	if act.Name != "" && err != nil {
+		return fmt.Errorf("invalid step found, missing parameters for step types")
+	}
+	return fmt.Errorf("invalid step found with no name, missing parameters for step types")
 }
 
 // Failed returns a slice of strings containing the names of failed steps in the TTP.
@@ -101,24 +107,20 @@ func (t *TTP) fetchEnv() {
 	}
 }
 
-// RunSteps executes all of the steps in the given TTP.
-//
-// Parameters:
-//
-// t: The TTP to execute the steps for.
-//
-// Returns:
-//
-// error: An error if any of the steps fail to execute.
-func (t *TTP) RunSteps() error {
-	if t.WorkDir == "" {
-		path, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		t.WorkDir = path
+func (t *TTP) setWorkingDirectory() error {
+	if t.WorkDir != "" {
+		return nil
 	}
 
+	path, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	t.WorkDir = path
+	return nil
+}
+
+func (t *TTP) validateSteps() error {
 	logging.Logger.Sugar().Info("[*] Validating Steps")
 
 	for _, step := range t.Steps {
@@ -131,14 +133,14 @@ func (t *TTP) RunSteps() error {
 			return err
 		}
 	}
-
-	t.fetchEnv()
 	logging.Logger.Sugar().Info("[+] Finished validating steps")
+	return nil
+}
 
+func (t *TTP) executeSteps() (map[string]Step, []CleanupAct, error) {
 	logging.Logger.Sugar().Infof("[+] Running current TTP: %s", t.Name)
 	availableSteps := make(map[string]Step)
 	var cleanup []CleanupAct
-	var err error
 
 	for _, step := range t.Steps {
 		stepCopy := step
@@ -147,23 +149,38 @@ func (t *TTP) RunSteps() error {
 
 		if err := stepCopy.Execute(); err != nil {
 			logging.Logger.Sugar().Errorw("error encountered in stepCopy execution: %v", err)
-			break
+			return nil, nil, err
 		}
 		// Enters in reverse order
 		availableSteps[stepCopy.StepName()] = stepCopy
-
-		logging.Logger.Sugar().Debugw("step data", "data", stepCopy)
-		stepClean := stepCopy.GetCleanup()
-		if len(stepClean) > 0 {
-			logging.Logger.Sugar().Debugw("adding cleanup step", "cleanup", stepClean)
-			cleanup = append(stepClean, cleanup...)
-		}
-		logging.Logger.Sugar().Debugw("available step data", "data", availableSteps[stepCopy.StepName()].GetOutput())
+		cleanup = append(stepCopy.GetCleanup(), cleanup...)
 		logging.Logger.Sugar().Infof("[+] Finished running step: %s", step.StepName())
 	}
-	// original error from step loop
+	return availableSteps, cleanup, nil
+}
+
+// RunSteps executes all of the steps in the given TTP.
+//
+// Parameters:
+//
+// t: The TTP to execute the steps for.
+//
+// Returns:
+//
+// error: An error if any of the steps fail to execute.
+func (t *TTP) RunSteps() error {
+	if err := t.setWorkingDirectory(); err != nil {
+		return err
+	}
+
+	if err := t.validateSteps(); err != nil {
+		return err
+	}
+
+	t.fetchEnv()
+
+	availableSteps, cleanup, err := t.executeSteps()
 	if err != nil {
-		logging.Logger.Sugar().Errorw("error encountered in step loop: %v", err)
 		return err
 	}
 
@@ -183,6 +200,21 @@ func (t *TTP) RunSteps() error {
 	return nil
 }
 
+func (t *TTP) executeCleanupSteps(availableSteps map[string]Step, cleanupSteps []CleanupAct) error {
+	for _, step := range cleanupSteps {
+		stepCopy := step
+		logging.Logger.Sugar().Infof("[+] Running current cleanup step: %s", step.CleanupName())
+		stepCopy.Setup(t.Environment, availableSteps)
+
+		if err := stepCopy.Cleanup(); err != nil {
+			logging.Logger.Sugar().Errorw("error encountered in stepCopy cleanup: %v", err)
+			return err
+		}
+		logging.Logger.Sugar().Infof("[+] Finished running cleanup step: %s", step.CleanupName())
+	}
+	return nil
+}
+
 // Cleanup executes all of the cleanup steps in the given TTP.
 //
 // Parameters:
@@ -192,26 +224,11 @@ func (t *TTP) RunSteps() error {
 // Returns:
 //
 // error: An error if any of the cleanup steps fail to execute.
-func (t *TTP) Cleanup(availableSteps map[string]Step, cleanupSteps []CleanupAct) (err error) {
-	for _, step := range cleanupSteps {
-		stepCopy := step
-		logging.Logger.Sugar().Infof("[+] Running current cleanup step: %s", step.CleanupName())
-		stepCopy.Setup(t.Environment, availableSteps)
-
-		if err := stepCopy.Cleanup(); err != nil {
-			logging.Logger.Sugar().Errorw("error encountered in stepCopy cleanup: %v", err)
-			break
-		}
-		// Enters in reverse order
-		logging.Logger.Sugar().Infof("[+] Finished running cleanup step: %s", step.CleanupName())
-
-	}
-
-	// original error from cleanup step loop
+func (t *TTP) Cleanup(availableSteps map[string]Step, cleanupSteps []CleanupAct) error {
+	err := t.executeCleanupSteps(availableSteps, cleanupSteps)
 	if err != nil {
 		logging.Logger.Sugar().Errorw("error encountered in cleanup step loop: %v", err)
 		return err
 	}
-
 	return nil
 }
