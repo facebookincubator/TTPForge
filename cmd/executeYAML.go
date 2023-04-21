abde3bade33a527bb503bded587757970e616c21
Sam <106620054+d3sch41n@users.noreply.github.com>
2023-04-21 11:51:32 -0500

    Add E2E Test Support and Bugfixes (#58)
    * Add E2E test framework for YAML TTP execution 
    * Add vscode debugger support for E2E tests
    * Identify and fix critical unmarshal bug
    * Optimize pre-commit

---
+++ cmd/executeYAML.go
@@ -0,0 +1,81 @@
+/*
+Copyright © 2023-present, Meta Platforms, Inc. and affiliates
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package cmd
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/facebookincubator/ttpforge/pkg/blocks"
+	"github.com/facebookincubator/ttpforge/pkg/logging"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+// ExecuteYAML is the top-level TTP execution function
+// exported so that we can test it
+// the returned TTP is also required to assert against in tests
+func ExecuteYAML(yamlFile string) (*blocks.TTP, error) {
+	logging.Logger = Logger
+
+	ttp, err := blocks.LoadTTP(yamlFile)
+	if err != nil {
+		Logger.Sugar().Errorw("failed to run TTP", zap.Error(err))
+		return nil, err
+	}
+
+	inventory := viper.GetStringSlice("inventory")
+	Logger.Sugar().Debugw("Inventory path gathered", "paths", inventory)
+
+	for _, path := range inventory {
+		dir, err := os.Getwd()
+		if err != nil {
+			return nil, err
+		}
+		abs, err := blocks.FetchAbs(path, dir)
+		if err != nil {
+			return nil, err
+		}
+		blocks.InventoryPath = append(blocks.InventoryPath, abs)
+	}
+
+	if len(inventory) > 0 {
+		blocks.InventoryPath = inventory
+	} else {
+		Logger.Sugar().Warn("No inventory path specified, using current directory")
+	}
+
+	// Use the directory of the YAML file as the full path reference.
+	dir := filepath.Dir(yamlFile)
+	dir, err = filepath.Abs(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the working directory for the TTP.
+	ttp.WorkDir = dir
+
+	if err := ttp.RunSteps(); err != nil {
+		Logger.Sugar().Errorw("failed to run TTP", zap.Error(err))
+		return nil, err
+	}
+
+	return &ttp, nil
+}
--- cmd/executeYAML_test.go
+++ cmd/executeYAML_test.go
@@ -17,12 +17,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-package main
+package cmd
 
 import (
-	"github.com/facebookincubator/ttpforge/cmd"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func main() {
-	cmd.Execute()
+func runE2ETest(t *testing.T, testFile string, stepOutputs []string) {
+	ttp, err := ExecuteYAML("e2e-tests/" + testFile)
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(stepOutputs), len(ttp.Steps), "step outputs should have correct length")
+
+	for stepIdx, step := range ttp.Steps {
+		output := step.GetOutput()
+		b, err := json.Marshal(output)
+		assert.Nil(t, err)
+		assert.Equal(t, stepOutputs[stepIdx], string(b), "step output is incorrect")
+
+	}
+
+	assert.NotNil(t, ttp)
+}
+
+func TestE2E(t *testing.T) {
+
+	dirname, err := os.UserHomeDir()
+	assert.Nil(t, err)
+
+	scenarios := map[string][]string{
+		"test_variable_expansion.yaml": {
+			fmt.Sprintf("{\"output\":\"%v\"}", dirname),
+			fmt.Sprintf("{\"another_key\":\"wut\",\"test_key\":\"%v\"}", dirname),
+			"{\"output\":\"you said: wut\"}",
+		},
+	}
+	for scenarioFile, stepOutputs := range scenarios {
+		runE2ETest(t, scenarioFile, stepOutputs)
+	}
 }
