163da330b69aac8f5b5a150645adfe618d0d23a0
Jayson Grace <jayson.e.grace@gmail.com>
2023-10-10 10:47:26 -0600

    Update magefile_test with final changes to compile command

--- magefiles/scripts/magefile_test.sh
+++ magefiles/scripts/magefile_test.sh
@@ -6,17 +6,17 @@ echo "Starting the test of magefile commands..."
 
 # Compile for all supported OS and architectures
 echo "Testing 'Compile' for all supported OS and architectures..."
-mage Compile true
+release=true mage Compile
 
 # Compile for specific OS and architectures
 echo "Testing 'Compile' for macOS on arm64..."
-GOOS=darwin GOARCH=arm64 mage Compile false
+GOOS=darwin GOARCH=arm64 release=false mage compile
 
 echo "Testing 'Compile' for Linux on amd64..."
-GOOS=linux GOARCH=amd64 mage Compile false
+GOOS=linux GOARCH=amd64 release=false mage compile
 
 echo "Testing 'Compile' for Windows on amd64..."
-GOOS=windows GOARCH=amd64 mage Compile false
+GOOS=windows GOARCH=amd64 release=false mage compile
 
 # Install Dependencies
 echo "Testing 'InstallDeps'..."
