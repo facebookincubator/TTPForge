a3b5ad81bf17e05dbc0f5448cdec07e40ad033b8
Ryan Edward Kozak <d0n601@meta.com>
2024-03-13 08:00:46 -0700

    Update run-all-ttp-tests.sh (#492)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/TTPForge/pull/492
    
    Update script to iterate over command line arguments in order to pass multiple directories for validation.
    
    Reviewed By: d3sch41n
    
    Differential Revision: D54753268
    
    fbshipit-source-id: 6487485ded83fb6ef2a33bc7a162cd9ff831eb81

--- run-all-ttp-tests.sh
+++ run-all-ttp-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# validate first argument
+# Validate path to TTPForge binary
 TTPFORGE_BINARY="$1"
 if [ ! -f "${TTPFORGE_BINARY}" ]
 then
@@ -10,17 +10,20 @@ then
 fi
 TTPFORGE_BINARY=$(realpath "${TTPFORGE_BINARY}")
 
-# validate second argument
-TTP_DIR="$2"
-if [ ! -d "${TTP_DIR}" ]
-then
-  echo "Invalid TTP Directory Specified!"
-  exit 1
-fi
-TTP_DIR=$(realpath "${TTP_DIR}")
+# Loop over all specified directories and validate all ttps within each.
+shift
+for TTP_DIR in "$@"; do
+  # validate directory
+  if [ ! -d "${TTP_DIR}" ]
+  then
+    echo "Invalid TTP Directory Specified!"
+    exit 1
+  fi
+  TTP_DIR=$(realpath "${TTP_DIR}")
 
-TTP_FILE_LIST="$(find "${TTP_DIR}" -name "*.yaml")"
-for TTP_FILE in ${TTP_FILE_LIST}
-do
-    ${TTPFORGE_BINARY} test "${TTP_FILE}"
+  TTP_FILE_LIST="$(find "${TTP_DIR}" -name "*.yaml")"
+  for TTP_FILE in ${TTP_FILE_LIST}
+  do
+      ${TTPFORGE_BINARY} test "${TTP_FILE}"
+  done
 done
